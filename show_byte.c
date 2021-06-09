0e5417ec7cdfc896c5904c55273dd34d6de0b1cb
Le Sang <sang.le@mservice.com.vn>
2021-06-09 11:40:23 +0700

    printf specified from hexa value

--- show_byte.c
+++ show_byte.c
@@ -37,4 +37,10 @@ void test_show_bytes(int val) {
 
 int main() {
     test_show_bytes(2);
+
+    int a = 0x12345678;
+    byte_pointer ap = (byte_pointer) &a;
+    show_bytes(ap, 1);
+    show_bytes(ap, 2);
+    show_bytes(ap, 3);
 }
