5fc39c0c87f4d5ccee5a773b696215b5727f393a
Le Sang <sang.le@mservice.com.vn>
2021-06-09 11:19:27 +0700

    initialize code

---
+++ show_byte.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    int i;
+    for (i = 0; i < len; ++i) {
+        printf("%.2x", start[i]);
+    }
+
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(x));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(x));
+}
+
+void show_pointer(void * x) {
+    show_bytes((byte_pointer) &x, sizeof(x));
+}
+
+int main() {
+    int x = 2;
+    float x2 = 2.0;
+    void *x3 = &x2;  
+    printf("\nShow bytes of int 2: ");
+    show_int(x);
+    printf("\nShow bytes of float 2.0 at %.2x : ", &x2);
+    show_float(x2);
+    printf("\nShow bytes of pointer void * :");
+    show_pointer(x3);
+}
